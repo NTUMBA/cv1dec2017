32df678238b8a3acf1543db4579ee7743522b907
NTUMBA <cityzenmboma@yahoo.fr>
2018-02-03 18:20:15 +0100

    Adding JS's fading effect and style's background linear-gradient

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(main);
+
+function main() {
+  $('.rectangle1').hide();
+  $('.rectangle1').fadeIn(6000);
+  $('.rectangle2').hide();
+  $('.rectangle2').fadeIn(10000);
+  $('.rectangle3').hide();
+  $('.rectangle3').fadeIn(14000);
+  $('.rectangle4').hide();
+  $('.rectangle4').fadeIn(18000);
+  $('.arrow-right').hide();
+  $('.arrow-right').fadeIn(7000);
+  $('.arrow-right_2').hide();
+  $('.arrow-right_2').fadeIn(11000);
+  $('.arrow-right_3').hide();
+  $('.arrow-right_3').fadeIn(15000);
+  $('.arrow-right_4').hide();
+  $('.arrow-right_4').fadeIn(19000);
+  $('.mission').hide();
+  $('.mission').fadeIn(6000);
+}
