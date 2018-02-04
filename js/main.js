5bfd4088e990955a38674ea299d52b1562ea557f
NTUMBA <cityzenmboma@yahoo.fr>
2018-02-04 03:35:26 +0100

    Fixing transition duration

--- js/main.js
+++ js/main.js
@@ -2,21 +2,21 @@ $(document).ready(main);
 
 function main() {
   $('.rectangle1').hide();
-  $('.rectangle1').fadeIn(6000);
+  $('.rectangle1').fadeIn(3000);
   $('.rectangle2').hide();
-  $('.rectangle2').fadeIn(10000);
+  $('.rectangle2').fadeIn(5000);
   $('.rectangle3').hide();
-  $('.rectangle3').fadeIn(14000);
+  $('.rectangle3').fadeIn(7000);
   $('.rectangle4').hide();
-  $('.rectangle4').fadeIn(18000);
+  $('.rectangle4').fadeIn(9000);
   $('.arrow-right').hide();
-  $('.arrow-right').fadeIn(7000);
+  $('.arrow-right').fadeIn(5000);
   $('.arrow-right_2').hide();
-  $('.arrow-right_2').fadeIn(11000);
+  $('.arrow-right_2').fadeIn(7000);
   $('.arrow-right_3').hide();
-  $('.arrow-right_3').fadeIn(15000);
+  $('.arrow-right_3').fadeIn(9000);
   $('.arrow-right_4').hide();
-  $('.arrow-right_4').fadeIn(19000);
+  $('.arrow-right_4').fadeIn(11000);
   $('.mission').hide();
-  $('.mission').fadeIn(6000);
+  $('.mission').fadeIn(14000);
 }
